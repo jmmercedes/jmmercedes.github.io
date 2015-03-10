65ea9d1c9bdf617527bf748c28e5493e29e1df68
jmmercedes <jmercedes@kippnyccp.org>
2015-03-10 13:35:50 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   var my_array ["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8","9","9","10","10"];
-  for(i=1; i<21; i++){
+  for(i in my_array){
     $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
-  }
-});
+  };
     $('card').click(function(){
-      $(this). find("p").css("opacity;1")      
-          }
+      $(this). find("p").css("opacity,1");
+      $(this).html()
+       }
