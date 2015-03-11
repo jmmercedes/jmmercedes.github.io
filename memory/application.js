cb749ae94e974227eb82639f7887815be44b36f9
jmmercedes <jmercedes@kippnyccp.org>
2015-03-11 13:29:53 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
   var my_array ["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8","9","9","10","10"];
+  function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
   for(i in my_array){
     $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
   };
