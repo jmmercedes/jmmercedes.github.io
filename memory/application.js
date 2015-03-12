2c8d50d9e06f914624bb5d50b2d107c7a485bb7a
jmmercedes <jmercedes@kippnyccp.org>
2015-03-12 13:09:56 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-  var my_array ["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8","9","9","10","10"];
+  var click_count = 0;
+  var myarray = ["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8","9","9","10","10"];
+  for(i in myarray){
+    $('#card_holder').append('<div class="card"><p>'+ myarray[i]+'</p></div>');
+  } 
   function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex ;
 
@@ -18,11 +22,24 @@ $(document).ready(function(){
 
   return array;
 }
- var new_array = shuffle(my_array);
-  for(i in my_array){
-    $('#card_holder').append('<div class="card"><p>'+my_array(i)+'</p></div>');
-  };
-    $('card').click(function(){
-      $(this).find("p").css("opacity, 1");
-      $(this).html();
-       }
+  //$(this).css("border","1px");
+    $('.card').on("click", function(){
+        if(click_count == 0) {
+            $(this).find('p').css('opacity', 1).addClass('clicked');
+            value1=$(this).find('p').html();
+            click_count=1;
+        }
+        else{
+            $(this).find('p').css('opacity', 1).addClass('clicked');
+            value2=$(this).find('p').html();
+            click_count=0;
+            if(value1 == value2){
+              $('.clicked').css('opacity', 1).removeClass('clicked').addClass('correct');
+            }
+            else{
+                $('.clicked').css('opacity', 0).removeClass('clicked');
+            }
+        }
+    })
+
+});
