024a17e6598d9eacb53db54fa5da8a93ed97d46c
jmmercedes <jmercedes@kippnyccp.org>
2015-05-11 13:35:46 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,9 +1,8 @@
  from random import randint
-num={2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0}
-#pro=roll/500
-for i in range(500):
-   roll=randint(1,6)+randint(1,6)
-   num[roll] += 1
+win=0
+for i in range(10000):
+   regions = 0
+   votes = randint(1,100)
    
 for item in num:
    pro = num[item]/500
