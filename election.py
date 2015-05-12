f0c67f06eb848e105f73736c3cff5bd59940ee1e
jmmercedes <jmercedes@kippnyccp.org>
2015-05-12 12:52:53 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,9 +1,13 @@
- from random import randint
-win=0
-for i in range(10000):
-   regions = 0
-   votes = randint(1,100)
-   
-for item in num:
-   pro = num[item]/500
-   print pro   
+ from random import radint
+candidate_1=0
+for i in range(10000);
+regions = 0
+if randint(1,100) <= 87;#picks percentage and then checks if they won the region
+  regions += 1
+if randint (1,100) <=65;
+  regions += 1
+if randint (1,100) <=17;
+  regions +=1
+if region >= 2;
+  candidate_1 += 1
+print "the chances of winning are" + "candidate_1/10000"#prints out the chances of the candidate winning
