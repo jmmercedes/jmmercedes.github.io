045eccf1841ba9da9f15276719c854f8c3583709
jmmercedes <jmercedes@kippnyccp.org>
2015-05-12 13:17:31 -0400

    Update election.py

--- election.py
+++ election.py
@@ -2,12 +2,12 @@
 candidate_1=0
 for i in range(10000);
 regions = 0
-if randint(1,100) <= 87;#picks percentage and then checks if they won the region
+if randint(1,100) <= 87;#this helps picks percentage and then checks if they won the region
   regions += 1
-if randint (1,100) <=65;
+if randint (1,100) <=65;#till line 11 is the possiblity of winning
   regions += 1
 if randint (1,100) <=17;
   regions +=1
 if region >= 2;
   candidate_1 += 1
-print "the chances of winning are" + "candidate_1/10000"#prints out the chances of the candidate winning
+print "the chances of winning are" + str(candidate_1/10000)#this prints out the number of chances of the candidate winning
