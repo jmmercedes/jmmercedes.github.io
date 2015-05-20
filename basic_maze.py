f483b4e0c09bae000a16a7c9f3efd268a4654492
jmmercedes <jmercedes@kippnyccp.org>
2015-05-20 13:07:11 -0400

    Update basic_maze.py

--- basic_maze.py
+++ basic_maze.py
@@ -1,20 +1,28 @@
-from gipigo import
+from gipigo import *
 import time
 import math
 
 set_speed(100)
-right () 
-enc_tgt(0,1,14)
-enc_(1,1, floor (1219.2))
-time.sleep(.1)
-fwd()  
-time.sleep(3)
+#right () 
+#enc_tgt(0,1,14)
+#enc_(1,1, floor (1219.2))
+#time.sleep(.1)
+#fwd()  
+#time.sleep(3)
 
 def move_forward(feet):
-  mm+304.8 * feet
+  mm = 304.8 * feet
+  steps = mm/11.34
+  enc_tgt(1,1,steps)
+  time.sleep(.1)
+  fwd()
+  time.sleep(feet*2)
 
-move forward (3)
+def turn_left():
+  enc_tgt(0,1,14)
 left()
-enc_tgt(0,1,14)
+enc_tgt(0,1,15)
+time.sleep(.1)
+fwd()
 enc_(1,1, floor (1219.2))
 time.sleep(3)
