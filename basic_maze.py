cb86b5625bcf0e364efea30a5d0e66c02127ffe7
jmmercedes <jmercedes@kippnyccp.org>
2015-05-20 12:43:02 -0400

    Create basic_maze.py

---
+++ basic_maze.py
@@ -0,0 +1,20 @@
+from gipigo import
+import time
+import math
+
+set_speed(100)
+right () 
+enc_tgt(0,1,14)
+enc_(1,1, floor (1219.2))
+time.sleep(.1)
+fwd()  
+time.sleep(3)
+
+def move_forward(feet):
+  mm+304.8 * feet
+
+move forward (3)
+left()
+enc_tgt(0,1,14)
+enc_(1,1, floor (1219.2))
+time.sleep(3)
