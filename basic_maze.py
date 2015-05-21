e15c6e8e8d8ffd9fa08e76911fd115977e5ffaf9
jmmercedes <jmercedes@kippnyccp.org>
2015-05-21 13:14:44 -0400

    Update basic_maze.py

--- basic_maze.py
+++ basic_maze.py
@@ -9,6 +9,14 @@ set_speed(100)
 #time.sleep(.1)
 #fwd()  
 #time.sleep(3)
+def turn_left():
+  enc_tgt(0,1,14)
+  left()
+  enc_tgt(0,1,15)
+  time.sleep(.1)
+  fwd()
+  enc_(1,1, floor (1219.2))
+  time.sleep(3)
 
 def move_forward(feet):
   mm = 304.8 * feet
@@ -18,11 +26,26 @@ def move_forward(feet):
   fwd()
   time.sleep(feet*2)
 
+
+def turn_right():
+  enc_tgt(1,0,15)
+  time.sleep(.1)
+  right()
+  time.sleep(2)
+
 def turn_left():
-  enc_tgt(0,1,14)
-left()
-enc_tgt(0,1,15)
-time.sleep(.1)
-fwd()
-enc_(1,1, floor (1219.2))
-time.sleep(3)
+  enc_tgt(0,1,15)
+  time.sleep(.1)
+  left()
+  time.sleep(2)
+
+
+move_forward(4)
+turn_right()
+move_forward(3)
+turn_right()
+move_forward(2)
+turn_left()
+move_forward(1)
+turn_left()
+move_forward(3)
