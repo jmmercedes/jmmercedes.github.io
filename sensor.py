71ed2d56e2531fd472053997a0848a2b32fd07bf
jmmercedes <jmercedes@kippnyccp.org>
2015-05-27 13:39:17 -0400

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,3 +1,5 @@
+from gopigo import*
+
 def turn_right():
   enc_tgt(1,0,15)
   time.sleep(.1)
@@ -9,19 +11,9 @@ def turn_left():
   time.sleep(.1)
   left()
   time.sleep(2)
-
-
-move_forward(4)
-turn_right()
-move_forward(3)
-turn_right()
-move_forward(2)
-turn_left()
-move_forward(1)
-turn_left()
-move_forward(3)
-
-if us_dist(15)<50 
-  turn_left()
-else us_dist(15)>50
-  fwd()
+  
+while 1<2:
+  if us_dist(15)<50: 
+    turn_left()
+  else:
+    fwd()
